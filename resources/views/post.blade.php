7db18ab43c1264876340952219d77ca6942e7c80
MuhammadAsharul <masharul51@gmail.com>
2022-07-04 10:57:56 +0700

    update Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,7 @@
 @section('container')
     <h2 class="mb-5">{{ $post->title }}</h2>
     {{-- {{ $post->body}} --}}
+    <p>By Asharul <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
     {!! $post->body!!}
 
     <a href="/posts">Back to Post</a>
